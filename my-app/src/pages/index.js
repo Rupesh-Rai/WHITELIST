f4915dcc1f278a66f0e2fa34d450fb01c661d382
Rupesh-Rai <rairairupes001@gmail.com>
2023-03-18 18:19:25 +0545

    dApp

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
-import { providers, Contract } from "ethers";
+import { providers, Contract} from "ethers";
 import { useEffect, useRef, useState } from "react";
 import { WHITELIST_CONTRACT_ADDRESS, abi } from "../../constants/index";
 
